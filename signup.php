9ebdaec65ad6dbab25aab98c84e947722cb65696
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-13 14:34:43 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -455,6 +455,11 @@
 </div>
 </div>
 </div>
+<footer class="footer mt-auto py-3 bg-light">
+      <div class="container text-center">
+        <span class="text-muted">Photography Website &copy; 2024</span>
+      </div>
+    </footer>
 </body>
 </html>
 <!--<script src="node_modules/bootstrap/js/validation.js"></script>-->
