8b89eba9af3a860485abe168e03e8cb0a0b602f9
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-13 12:39:38 -0500

    Create login.php
    Started login.php

---
+++ login.php
@@ -0,0 +1,82 @@
+<html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Photography Website</title>
+    <link
+      href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <style>
+      .custom-navbar {
+        background-color: #fdf4eb;
+      }
+
+      body {
+        font-family: "Georgia", sans-serif;
+      }
+
+      .dropdown-menu {
+        display: none;
+      }
+
+      #hover-dropdown:hover .dropdown-menu {
+        display: block;
+        min-width: 1rem;
+        max-width: 6.5rem;
+        max-height: calc(50vh - 50px);
+        overflow-y: auto;
+        text-align: center;
+      }
+
+      .photo-row {
+        padding: 50px;
+      }
+
+      .photo-row img {
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+        padding: 20px;
+        margin-right: 20px;
+      }
+
+      .category {
+        margin-bottom: 10px;
+        padding: 40px;
+        margin-right: 10px;
+      }
+
+      .custom-button {
+        background-color: #fdf4eb;
+        border-color: #fdf4eb;
+        color: #a5998c;
+      }
+
+      .custom-button:hover {
+        background-color: #f0e6d1;
+        border-color: #f0e6d1;
+        color: #a5998c;
+      }
+    </style>
+  </head>
+
+
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</html>
