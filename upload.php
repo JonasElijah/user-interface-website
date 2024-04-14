3308d636318f27cab6ca70d79af234e37e989f24
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-14 12:41:21 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -187,8 +187,14 @@
           <div class="card-body">
             <h5 class="card-title">Upload Your Image</h5>
             <p class="card-text">Please select an image from your device to upload.</p>
+            <label for="category"> Category : </label>
+            <input type="text" name="category" id = "catergory" required value=""> <br>
+            <label for="desc"> Description : </label>
+            <input type="text" name="desc" id = "desc" required value=""> <br>
+            <label for="price"> Price : </label>
+            <input type="number" name="price" id = "price" required value=""> <br>
             <label for="upload" class="custom-file-label">Choose File</label>
-            <input type="file" id="upload" class="custom-file-input">
+            <input type="file" id="upload" accept=".jpg, .jpeg, .png" class="custom-file-input">
             <button class="btn btn-primary">Upload</button>
           </div>
         </div>
