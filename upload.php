4a4bc3a9c71566832707b12c029f90dfe9e984ee
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-14 13:16:47 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,54 +1,46 @@
 <?php
 require 'functions.php';
-if(isset($_POST["submit"])){
+
+if(isset($_POST["submit"])) {
   $name = $_POST["name"];
-  if($_FILES["image"]["error"] == 4){
-    echo
-    "<script> alert('Image Does Not Exist'); </script>"
-    ;
-  }
-  else{
+  $category = $_POST["category"];
+  $price = $_POST["price"];
+  $desc = $_POST["desc"];
+
+  // Check if an image was uploaded
+  if($_FILES["image"]["error"] == 4) {
+    echo "<script>alert('Image Does Not Exist');</script>";
+  } else {
     $fileName = $_FILES["image"]["name"];
     $fileSize = $_FILES["image"]["size"];
     $tmpName = $_FILES["image"]["tmp_name"];
 
-    $validImageExtension = ['jpg', 'jpeg', 'png'];
-    $imageExtension = explode('.', $fileName);
-    $imageExtension = strtolower(end($imageExtension));
-    if ( !in_array($imageExtension, $validImageExtension) ){
-      echo
-      "
-      <script>
-        alert('Invalid Image Extension');
-      </script>
-      ";
-    }
-    else if($fileSize > 1000000){
-      echo
-      "
-      <script>
-        alert('Image Size Is Too Large');
-      </script>
-      ";
-    }
-    else{
-      $newImageName = uniqid();
-      $newImageName .= '.' . $imageExtension;
+    // Validate image extension
+    $validImageExtensions = ['jpg', 'jpeg', 'png'];
+    $imageExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+    if (!in_array($imageExtension, $validImageExtensions)) {
+      echo "<script>alert('Invalid Image Extension');</script>";
+    } else if ($fileSize > 1000000) { // Validate image size
+      echo "<script>alert('Image Size Is Too Large');</script>";
+    } else {
+      // Generate a unique name for the image
+      $newImageName = uniqid() . '.' . $imageExtension;
 
+      // Move the uploaded image to the img directory
       move_uploaded_file($tmpName, 'img/' . $newImageName);
-      $query = "INSERT INTO tb_upload VALUES('', '$name', '$newImageName')";
-      mysqli_query($conn, $query);
-      echo
-      "
-      <script>
-        alert('Successfully Added');
-        document.location.href = 'data.php';
-      </script>
-      ";
+
+      // Insert the image information into the database
+      $query = "INSERT INTO your_table_name (name, category, price, `desc`, image) VALUES ('$name', '$category', $price, '$desc', '$newImageName')";
+      if(mysqli_query($conn, $query)) {
+        echo "<script>alert('Successfully Added'); document.location.href = 'data.php';</script>";
+      } else {
+        echo "<script>alert('Error: " . mysqli_error($conn) . "');</script>";
+      }
     }
   }
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
