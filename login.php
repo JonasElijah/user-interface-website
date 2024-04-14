4ba318225844f13b6dfedf6818cd19452d4245cf
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-14 10:27:26 -0500

    Update login.php

--- login.php
+++ login.php
@@ -144,7 +144,7 @@
 </div>
 
 <div align = "center">
-<form class = "col-md-6 col-md-offset-3 form-horizontal">
+<form class = "col-md-6 col-md-offset-3 form-horizontal form">
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
