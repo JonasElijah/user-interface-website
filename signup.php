9e271a2dace32244c9f3d0b57780c75bf75b7c46
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-14 13:13:25 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -115,13 +115,22 @@
               <li class="nav-item">
                 <a class="nav-link" href="#">About</a>
               </li>
+		    <?php
+			    if(!isset($_SESSION['userID'])
+			       {
+			echo '<li class="nav-item"> ';
+               	 	echo ' <a class="nav-link" href="#">Login</a>';
+              		echo '</li>';
+              		echo '<li class="nav-item">';
+                	echo '<a class="nav-link" href="signup.php">Signup</a>';
+              		echo ' </li>';
+			       }
+		?>
 		
               <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
+                <a class="nav-link" href="account.php">Account</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="signup.php">Signup</a>
-              </li>
+              
 
               <!--
               <li class="nav-item dropdown" id="hover-dropdown">
