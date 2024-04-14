73e8851f028025ad998a686adc8988158b8357fe
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-14 10:35:37 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Contact Form</title>
+<title>Sign Up Page</title>
 
 <link href="node_modules/css/bootstrap.min.css" rel="stylesheet">
 <link href="node_modules/css/bst-styles.css" rel="stylesheet">
