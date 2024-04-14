cade3893bb20b8defbdc0b2dd79434840f3153f3
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-14 17:24:53 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -31,7 +31,7 @@ if(isset($_POST["submit"])) {
       $newImageName = uniqid() . '.' . $imageExtension;
 
       // Move the uploaded image to the img directory
-      move_uploaded_file($tmpName, 'img' . $newImageName); 
+      move_uploaded_file($tmpName, 'img/' . $newImageName); 
 	
       // Insert the image information into the database
       $query = "INSERT INTO `image` (`category`, `price`, `ds`, `name`,  `image`) VALUES ( '$category', $price, '$desc', '$name', '$newImageName')";
