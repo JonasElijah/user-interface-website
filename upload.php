e546557fc517ef4e640c2aeba08d81f73098e51a
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-14 12:11:02 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -147,6 +147,17 @@
       </nav>
     </header>
     <br /><br /><br />
+    <div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+    </div>
+
+
+    
     <footer class="footer mt-auto py-3 bg-light">
       <div class="container text-center">
         <span class="text-muted">Photography Website &copy; 2024</span>
