4023c0dd98ad06aca6a0c3ce274541cfbf2a52be
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-14 11:43:35 -0500

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,20 @@
+<?php
+	function db_connect($database) {
+		$username="root";
+		$password="2024UTSA!";
+		$hostname="localhost";
+		$dblink=new mysqli($hostname,$username,$password,$database);
+		if(mysqli_connect_errno()){
+			die("Error connecting to database: ".mysqli_connect_error());
+		}
+		return $dblink;
+	}
+
+	function redirect($uri) {
+		?>
+		<script type="text/javascript">
+			document.location.href="<?php echo $uri; ?>";
+		</script>	
+		<?php die;
+	}
+?>
