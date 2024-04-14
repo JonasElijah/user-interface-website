fad86ed7d108b1eead10a5e181dabccd1f006fc1
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-14 12:33:42 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -78,7 +78,7 @@
         padding: 20px;
         border-radius: 10px;
         box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-        background-color: #ffffff;
+        background-color: #fdf4eb;
       }
       
       .custom-card .card-body 
