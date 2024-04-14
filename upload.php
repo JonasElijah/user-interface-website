4faea61e23f3a4ab3dbc3f85ecefcbc2664f4e8b
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-14 13:28:53 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -2,14 +2,9 @@
 
 include("functions.php");
 
-if (count($errStatus)>0)
-{
-	$errString=implode("&",$errStatus);
-	redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/signup.php?$errString");
-}
-/*			
+			
 $conn = db_connect("UI-schema");
-*/
+
 /*
 if(isset($_POST["submit"])) {
   $name = $_POST["name"];
