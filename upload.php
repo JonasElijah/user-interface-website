c0af6132caed5c3f570502a020c43709aabef775
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-14 18:16:41 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -236,6 +236,9 @@ echo "<img src='" . $_FILES["image"]["tmp_name"] . "' alt='Uploaded Image' style
         </div>
       </nav>
     </header>
+	     <?php if(isset($_POST["submit"]) && $_FILES["image"]["error"] == 0) { ?>
+        <img src="<?php echo $newImageName; ?>" alt="Uploaded Image" style="max-width: 250px; max-height: 100px;" />
+    <?php } ?>
     <br /><br /><br />
         <form class="" action="" method="post" autocomplete="off" enctype="multipart/form-data">
 	    <div class="upload">
