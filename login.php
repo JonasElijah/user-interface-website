d45abb9de2d34748b3cf1b49db013bd18647a118
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-14 10:15:10 -0500

    Update login.php

--- login.php
+++ login.php
@@ -152,7 +152,7 @@
   </div>
   <div class="form-group form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <label class="form-check-label" for="exampleCheck1">Keep me signed in.</label>
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
