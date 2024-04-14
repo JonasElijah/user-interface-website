fdf68c23c97d7494118551d8bec0990c3a571f49
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-14 13:03:57 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,3 +1,54 @@
+<?php
+require 'functions.php';
+if(isset($_POST["submit"])){
+  $name = $_POST["name"];
+  if($_FILES["image"]["error"] == 4){
+    echo
+    "<script> alert('Image Does Not Exist'); </script>"
+    ;
+  }
+  else{
+    $fileName = $_FILES["image"]["name"];
+    $fileSize = $_FILES["image"]["size"];
+    $tmpName = $_FILES["image"]["tmp_name"];
+
+    $validImageExtension = ['jpg', 'jpeg', 'png'];
+    $imageExtension = explode('.', $fileName);
+    $imageExtension = strtolower(end($imageExtension));
+    if ( !in_array($imageExtension, $validImageExtension) ){
+      echo
+      "
+      <script>
+        alert('Invalid Image Extension');
+      </script>
+      ";
+    }
+    else if($fileSize > 1000000){
+      echo
+      "
+      <script>
+        alert('Image Size Is Too Large');
+      </script>
+      ";
+    }
+    else{
+      $newImageName = uniqid();
+      $newImageName .= '.' . $imageExtension;
+
+      move_uploaded_file($tmpName, 'img/' . $newImageName);
+      $query = "INSERT INTO tb_upload VALUES('', '$name', '$newImageName')";
+      mysqli_query($conn, $query);
+      echo
+      "
+      <script>
+        alert('Successfully Added');
+        document.location.href = 'data.php';
+      </script>
+      ";
+    }
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
