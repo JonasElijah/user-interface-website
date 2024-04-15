1e5055452c8790b22e7f9fbf3639a704102197b7
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-15 14:06:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -112,7 +112,7 @@
                 <a class="nav-link" href="#">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Gallery</a>
+                <a class="nav-link" href="gallery.php">Gallery</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="#">FAQ</a>
