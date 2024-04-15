09de38c580036fb81e4aed2f41bc50d7e24f2f1f
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-15 14:52:03 -0500

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -279,6 +279,18 @@ if(isset($_POST["submit"])) {
 	    </div>
 	  </div>
 	</div>
+	     <?php 
+          $sql = "SELECT * FROM images ORDER BY id DESC";
+          $res = mysqli_query($conn,  $sql);
+
+          if (mysqli_num_rows($res) > 0) {
+          	while ($images = mysqli_fetch_assoc($res)) {  ?>
+             
+             <div class="alb">
+             	<img src="uploads/<?=$images['image_url']?>">
+             </div>
+          		
+    <?php } }?>
    <br />
     <br />
     <br />
