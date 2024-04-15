89152d7e45f42a24d637504ef090a4526473c906
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-14 19:40:25 -0500

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -223,52 +223,53 @@ if(isset($_POST["submit"])) {
 	<button type="button" data-bs-toggle="modal" data-bs-target="#myModal">Upload</button>
 	
 	<div class="modal fade custom-modal" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Upload Your Image here</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form class="" action="" method="post" autocomplete="off" enctype="multipart/form-data">
-                    <div class="upload">
-                        <div class="custom-card">
-                            <div class="card">
-                                <div class="card-body">
-                                    <p class="card-text">Please select an image from your device to upload.</p>
-                                    <div class="form-group">
-                                        <label for="name">Name:</label>
-                                        <input type="text" name="name" id="name" class="form-control" required value="">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="category">Category:</label>
-                                        <input type="text" name="category" id="category" class="form-control" required value="">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="desc">Description:</label>
-                                        <input type="text" name="desc" id="desc" class="form-control" required value="">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="price">Price:</label>
-                                        <input type="number" name="price" id="price" class="form-control" required value="">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="file" name="image" id="image" accept=".jpg, .jpeg, .png" value="" class="custom-file-label">
-                                    </div>
-                                    <button type="submit" name="submit">Submit</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </form>	        
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            </div>
-        </div>
-    </div>
-</div>
-
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <!-- Modal content goes here -->
+	      <div class="modal-header">
+	        <h5 class="modal-title" id="exampleModalLabel">Upload Your Image here</h5>
+	        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+	      </div>
+	      <div class="modal-body">
+	        <form class="" action="" method="post" autocomplete="off" enctype="multipart/form-data">
+	          <div class="upload">
+	            <div class="custom-card">
+	              <div class="card">
+	                <div class="card-body">
+	                  <p class="card-text">Please select an image from your device to upload.</p>
+	                  <div class="form-group">
+	                    <label for="name">Name:</label>
+	                    <input type="text" name="name" id="name" class="form-control" required value="">
+	                  </div>
+	                  <div class="form-group">
+	                    <label for="category">Category:</label>
+	                    <input type="text" name="category" id="category" class="form-control" required value="">
+	                  </div>
+	                  <div class="form-group">
+	                    <label for="desc">Description:</label>
+	                    <input type="text" name="desc" id="desc" class="form-control" required value="">
+	                  </div>
+	                  <div class="form-group">
+	                    <label for="price">Price:</label>
+	                    <input type="number" name="price" id="price" class="form-control" required value="">
+	                    <br>
+	                  </div>
+	                  <div class="form-group">
+	                    <input type="file" name="image" id="image" accept=".jpg, .jpeg, .png" value="" class="custom-file-label">
+	                  </div>
+			  <button type="submit" name="submit">Submit</button>
+	                </div>
+	              </div>
+	            </div>
+	          </div>
+	        </form>	        
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+	      </div>
+	    </div>
+	  </div>
+	</div>
 
 
    <br />
