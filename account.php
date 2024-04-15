69a67defa81d85ed9c7d933eb64dcb82a1d41013
Victoria Martinez <martinezvictoria549@gmail.com>
2024-04-15 14:15:33 -0500

    account debug

--- account.php
+++ account.php
@@ -1,6 +1,10 @@
 <?php
 	include("functions.php");
-
+	
+	if (session_status() === PHP_SESSION_NONE) {
+    	session_start();
+	}
+	
 	if(!isset($_SESSION['userID'])){
 		$_SESSION['userID']=6;
 		redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/login.php");
