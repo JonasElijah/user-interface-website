718f8698369dc4c1e7e7cccda17f712c0abb3155
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-16 10:00:16 -0500

    Update login.php

--- login.php
+++ login.php
@@ -275,7 +275,7 @@ if(!isset($_POST['submit'])){
 
 		
 		
-			echo '<br><button class="center-block btn btn-secondary" name="submit" type="submit" value="submit">Log In</button></form>';
+			echo '<br><button class="btn btn-success" align = "center" name="submit" type="submit" value="submit">Log In</button></form>';
 }
 	if(isset($_POST['submit'])){
 		echo '<div class ="col-md-6 col-md-offset-3">';
