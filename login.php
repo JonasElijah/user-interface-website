a72f1e6ba85f3e65f231bae2a5cbad8ed85a53da
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-16 14:31:59 -0500

    Update login.php

--- login.php
+++ login.php
@@ -113,10 +113,10 @@
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ms-auto">
               <li class="nav-item">
-                <a class="nav-link" href="#">Home</a>
+                <a class="nav-link" href="index.php">Home</a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="#">Gallery</a>
+                <a class="nav-link" href="gallery.php">Gallery</a>
               </li>
           
              <?php
