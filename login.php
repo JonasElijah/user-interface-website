870f5a4a6217faba349cd4e21720ddea08f3867b
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-16 09:56:40 -0500

    Update login.php

--- login.php
+++ login.php
@@ -353,4 +353,13 @@ if(!isset($_POST['submit'])){
 }
 		?>
 </body>
+<div>
+	<br>
+	<br>
+	<footer class="footer mt-auto py-3 bg-light">
+      <div class="container text-center">
+        <span class="text-muted">Photography Website &copy; 2024</span>
+      </div>
+    </footer>
+</div>
 </html>
