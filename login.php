94818e4b0f43bee79197e141dbfd2e308593087c
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-16 14:30:39 -0500

    Update login.php

--- login.php
+++ login.php
@@ -118,12 +118,7 @@
               <li class="nav-item">
                 <a class="nav-link" href="#">Gallery</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">FAQ</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">About</a>
-              </li>
+          
              <?php
 		if(!isset($_SESSION['userID'])){
 			echo '<li class="nav-item"> ';
