e18d867e9426be6d5049d9ff314398efe77e3c8c
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-16 15:21:48 -0500

    Update index.php

--- index.php
+++ index.php
@@ -222,7 +222,7 @@ if(mysqli_num_rows($result) == 0) {
                       <div class="card-body">
                         <h5 class="card-title">'.$imageName.'</h5>
                         <p class="card-text">'.$imagePrice.'</p>
-                        <button class="btn btn-primary">Add to Cart</button>
+                        <button class="btn btn-outline-secondary" name="submit" type="submit" value="submit">Add to Cart</button>
                       </div>
                     </div>
                   </div>';
@@ -244,6 +244,34 @@ if(mysqli_num_rows($result) == 0) {
           </button>
         </div>';
 }
+
+if(isset($_POST['submit']))
+		{	
+			if(!isset($_SESSION['userID']))
+			{
+				redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/login.php");
+			}
+			
+			$userID = $_SESSION['userID'];
+			$imageID = $_GET['itemID'];
+			$name = $data['name'];
+			$price = $data['price'];
+			
+			$sql="SELECT * FROM `orders` WHERE `userID` LIKE '$userID' AND `imageID` LIKE '$imageID'";
+			$result = mysqli_query($dblink, $sql);
+			if(mysqli_num_rows($result) == 0)
+			{
+				$sql="Insert into `orders` (`userID`,`imageID`,`name`,`price`) values ('$userID','$imageID','$name','$price')";
+				$dblink->query($sql) or
+					die("Something went wrong with: $sql<br>".$dblink->error."</p>");
+				
+				redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/view-item.php?addItem=success");
+			}
+			else
+			{
+				redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/view-item.php?addItem=failed");
+			}
+		}
 
  ?> 
 </div>
