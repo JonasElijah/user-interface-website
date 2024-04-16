64dd65fe0f1b845461926f9f47462f30c5078047
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-16 09:56:22 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -313,4 +313,13 @@ else
 	}
 ?>
 </body>
+<div>
+	<br>
+	<br>
+	<footer class="footer mt-auto py-3 bg-light">
+      <div class="container text-center">
+        <span class="text-muted">Photography Website &copy; 2024</span>
+      </div>
+    </footer>
+</div>
 </html>
