420aef8abe22ba17dc00fdf1951671ec543df9bf
Cody-Mangold <124631149+Cody-Mangold@users.noreply.github.com>
2024-04-16 13:19:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -168,7 +168,7 @@
 
 include("functions.php");
 $dblink = db_connect("UI-schema");
-$sql = "SELECT * FROM `image`"
+$sql = "SELECT * FROM `image`";
 $result = mysqli_query($dblink, $sql);
 if(mysqli_num_rows($result) == 0)
 		{
@@ -203,33 +203,7 @@ else
 
 
 ?>
-    <div>
-        <div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-	<a href
-      <img src="assets/images/gallery/DSC00892.jpg" alt="Your Logo" class="d-block w-100" style="max-width: 600px; height: auto;" />
-    </div>
-    <div class="carousel-item">
-      <img src="assets/images/gallery/DSC00868-Enhanced-NR.jpg" alt="Your Logo" class="d-block w-100" style="max-width: 600px; height: auto;" />
-    </div>
-    <div class="carousel-item">
-      <img src="assets/images/gallery/DSC00022.jpg" alt="Your Logo" class="d-block w-100" style="max-width: 600px; height: auto;" />
-    </div>
-    <div class="carousel-item">
-      <img src="assets/images/gallery/DSC00887.jpg" alt="Your Logo" class="d-block w-100" style="max-width: 600px; height: auto;" />
-    </div>
-  </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-
-</div>
+    
 
    <br />
     <br />
