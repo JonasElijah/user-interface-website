d6ef19617615cc64e300059236318e57374f9332
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-18 01:33:28 -0500

    Update view-item.php

--- view-item.php
+++ view-item.php
@@ -119,11 +119,11 @@
 		}
 
 	    .photo-cus {
-	  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Slightly larger shadow for more depth */
-	  padding: 20px; /* Keeps padding around the image */
-	  margin: 20px; /* Ensures there is space around each image */
-	  width: calc(100% - 40px); /* Adjust width to account for padding and maintain container size */
-	  height: auto; /* Maintain aspect ratio */
+		  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+		  padding: 20px; 
+		  margin: 20px; 
+		  width: 80%;
+		  height: auto; 
 	}
 
 		
