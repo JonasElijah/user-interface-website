8647d6ddda12ca2246863661b2274ffdd89e25b2
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-18 15:27:24 -0500

    Update index2.php

--- index2.php
+++ index2.php
@@ -254,49 +254,54 @@
                     return $img['category'] === 'portrait';
                 }
             ];
+		
+	    function isItemInCart($userId, $imageId, $dblink) {
+	        $query = "SELECT 1 FROM `orders` WHERE `userID` = '$userId' AND `imageID` = '$imageId'";
+	        $result = mysqli_query($dblink, $query);
+	        return mysqli_num_rows($result) > 0;
+	    }
 
-            function createCarouselItems($imageSets, $categoryName)
-            {
-                $carouselId = "carousel" . preg_replace('/\s+/', '', $categoryName);
-                echo '<h2>' . $categoryName . '</h2><hr>
-                      <div id="' . $carouselId . '" class="carousel slide" data-bs-ride="carousel">
-                      <div class="carousel-inner">';
-                $first = true;
-                foreach ($imageSets as $set) {
-                    $activeClass = $first ? 'active' : '';
-                    echo '<div class="carousel-item ' . $activeClass . '">
-                            <div class="row">';
-                    foreach ($set as $image) {
-                        $imagePath = $image['image'];
-                        $imageName = $image['name'];
-                        $imagePrice = $image['price'];
-                        $imageID = $image['ID'];
-			$userID = $_SESSION['userID'];
-			$query = "SELECT * FROM `orders` WHERE `imageID` = '$imageID' AND `userID` = '$userID'";
-			$res = mysqli_query($dblink, $query);
-                        echo '<div class="col-md-2">
-                    		<div class="card mb-3" style="cursor:pointer;" onclick="window.location.href=\'view-item.php?itemID=' . $imageID . '\'">
-                                  <img src="' . $imagePath . '" class="card-img-top" alt="Image of ' . $imageName . '" title="Click to view details">
-                                  <div class="card-body">
-                                    <h5 class="card-title">' . $imageName . '</h5>
-                                    <p class="card-text">$' . $imagePrice . '</p>
-				    <form method="post" action="">
-		                  	    <input type="hidden" name="imageID" value="' . $imageID . '"> 
-				            <input type="hidden" name="imageName" value="' . $imageName . '"> 
-		                  	    <input type="hidden" name="imagePrice" value="' . $imagePrice . '">';
-					    if (mysqli_num_rows($res) == 0) {
-					        echo '<button class="add-to-cart-btn" type="submit" name="submit">Add to Cart</button>';
-					    } else {
-						echo '<button type="button" class="btn btn-secondary" disabled>Button</button>';
-					    }
-				echo	'</form>
-                                  </div>
-                                </div>
-                              </div>';
-                    }
-                    echo '</div></div>';
-                    $first = false;
-                }
+
+            function createCarouselItems($imageSets, $categoryName, $userId, $dblink)
+		{
+		    $carouselId = "carousel" . preg_replace('/\s+/', '', $categoryName);
+		    echo '<h2>' . $categoryName . '</h2><hr>
+		          <div id="' . $carouselId . '" class="carousel slide" data-bs-ride="carousel">
+		          <div class="carousel-inner">';
+		    $first = true;
+		    foreach ($imageSets as $set) {
+		        $activeClass = $first ? 'active' : '';
+		        echo '<div class="carousel-item ' . $activeClass . '">
+		                <div class="row">';
+		        foreach ($set as $image) {
+		            $imagePath = $image['image'];
+		            $imageName = $image['name'];
+		            $imagePrice = $image['price'];
+		            $imageID = $image['ID'];
+		            $disabled = isItemInCart($userId, $imageID, $dblink) ? 'disabled' : '';
+		
+		            echo '<div class="col-md-2">
+		                  <div class="card mb-3">
+		                      <img src="' . $imagePath . '" class="card-img-top" alt="Image of ' . $imageName . '" title="Click to view details">
+		                      <div class="card-body">
+		                        <h5 class="card-title">' . $imageName . '</h5>
+		                        <p class="card-text">$' . $imagePrice . '</p>
+		                        <form method="post" action="">
+		                            <input type="hidden" name="imageID" value="' . $imageID . '"> 
+		                            <input type="hidden" name="imageName" value="' . $imageName . '"> 
+		                            <input type="hidden" name="imagePrice" value="' . $imagePrice . '"> 
+		                            <button class="add-to-cart-btn" type="submit" name="submit" ' . $disabled . '>Add to Cart</button>
+		                        </form>
+		                      </div>
+		                  </div>
+		                </div>';
+		        }
+		        echo '</div></div>';
+		        $first = false;
+		    }
+		    echo '</div>';
+		}
+
                 echo '</div>';
                 echo '
 		      <button class="carousel-control-prev" type="button" data-bs-target="#' . $carouselId . '" data-bs-slide="prev">
