ade6f63eaf2f0d9287a6738bf9610bc3eea9b39d
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-18 00:11:13 -0500

    Update index2.php

--- index2.php
+++ index2.php
@@ -334,17 +334,17 @@
                     echo '</div></div>';
                     $first = false;
                 }
-                echo '</div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#'.$carouselId.'" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#'.$carouselId.'" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span the="visually-hidden">Next</span>
-                </button>
-              </div>';
-            }
+                 echo '</div>
+	      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+	        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	        <span class="visually-hidden">Previous</span>
+	      </button>
+	      <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+	        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	        <span class="visually-hidden">Next</span>
+	      </button>
+	    </div>';
+	    }
 
             // Process each category
             foreach ($categories as $categoryName => $filter) {
