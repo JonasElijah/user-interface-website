2fb18f83cdd832699f0204e98c89dc4458261c43
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-18 17:48:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -240,7 +240,7 @@
 </head>
 <body>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg custom-navbar navbar-light bg-light">
 		  <div class="container-fluid">
 		    <a class="navbar-brand" href="#">Brand</a>
 		    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
