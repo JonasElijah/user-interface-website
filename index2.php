fc2f2079512f10019e0c3bb13d9483915f273862
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-18 00:19:22 -0500

    Update index2.php

--- index2.php
+++ index2.php
@@ -306,28 +306,34 @@
                       <div id="'.$carouselId.'" class="carousel slide" data-bs-ride="carousel">
                       <div class="carousel-inner">';
                 $first = true;
-               foreach($set as $image) {
-	            $imagePath = $image['image'];
-	            $imageName = $image['name'];
-	            $imagePrice = $image['price'];
-	            $imageID = $image['ID'];  // Assuming there's an 'id' field in your images table
-	
-	            echo '<div class="col-md-4">
-	                    <div class="card mb-3" style="cursor:pointer;" onclick="window.location.href=\'view-item.php?itemID='.$imageID.'\'">
-	                      <img src="'.$imagePath.'" class="card-img-top" alt="Image of '.$imageName.'" title="Click to view details">
-	                      <div class="card-body">
-	                        <h5 class="card-title">'.$imageName.'</h5>
-	                        <p class="card-text">'.$imagePrice.'</p>
-	                        <form method="post" action="">
-	                  	    <input type="hidden" name="imageID" value="'.$imageID.'"> 
-			            <input type="hidden" name="imageName" value="'.$imageName.'"> 
-	                  	    <input type="hidden" name="imagePrice" value="'.$imagePrice.'"> 
-				    <button type="submit" name="submit">Add to Cart</button>
-				</form>
-	                      </div>
-	                    </div>
-	                  </div>';
-	        }
+                foreach ($imageSets as $set) {
+                    $activeClass = $first ? 'active' : '';
+                    echo '<div class="carousel-item '.$activeClass.'">
+                            <div class="row">';
+                    foreach ($set as $image) {
+                        $imagePath = $image['image'];
+                        $imageName = $image['name'];
+                        $imagePrice = $image['price'];
+                        $imageID = $image['ID'];
+                        echo '<div class="col-md-2">
+                    		<div class="card mb-3" style="cursor:pointer;" onclick="window.location.href=\'view-item.php?itemID='.$imageID.'\'">
+                                  <img src="'.$imagePath.'" class="card-img-top" alt="Image of '.$imageName.'" title="Click to view details">
+                                  <div class="card-body">
+                                    <h5 class="card-title">'.$imageName.'</h5>
+                                    <p class="card-text">'.$imagePrice.'</p>
+				    <form method="post" action="">
+		                  	    <input type="hidden" name="imageID" value="'.$imageID.'"> 
+				            <input type="hidden" name="imageName" value="'.$imageName.'"> 
+		                  	    <input type="hidden" name="imagePrice" value="'.$imagePrice.'"> 
+					    <button type="submit" name="submit">Add to Cart</button>
+					</form>
+                                  </div>
+                                </div>
+                              </div>';
+                    }
+                    echo '</div></div>';
+                    $first = false;
+                }
                 echo '</div>'; // Close carousel-inner
                 echo '<button class="carousel-control-prev" type="button" data-bs-target="#'.$carouselId.'" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
