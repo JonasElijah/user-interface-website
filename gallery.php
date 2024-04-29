04b73791f4bb07515298c9f6c74354a7eb308fbb
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-29 16:20:35 -0500

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -225,7 +225,8 @@ $result = $conn->query($sql) or die("Something went wrong with: $sql<br>" . $con
 <div>
 <div class='gallery-title'>
 	<h1>Gallery (Keep photos less than a MB)</h1>
-	 <button type="button" data-bs-toggle="modal" data-bs-target="#myModal" class="nav-link">Upload</button>
+	<hr>
+	<button type="button" data-bs-toggle="modal" data-bs-target="#myModal" class="nav-link float-end">Upload</button>
      </div>
     <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-fullscreen-sm-down">
