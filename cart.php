4416b09ada17eb8fe348e5b66b0613b5804015aa
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-30 01:25:07 -0500

    Update cart.php

--- cart.php
+++ cart.php
@@ -260,16 +260,17 @@ if(!isset($_POST['submit']))
 							{
 						
 								echo '<td><img src = "'.$dataW['image'].'" style="max-width:250px;"></td>';
+								echo '<td>'.$data['name'].'</td>';
+								echo '<td>'.$data['price'].'</td>';
+								echo '</tr>';
+								$counter++;
+								$quantity++;
+								$sum += $data['price'];
 							}
 				
 					}
 			
-				echo '<td>'.$data['name'].'</td>';
-				echo '<td>'.$data['price'].'</td>';
-				echo '</tr>';
-				$counter++;
-				$quantity++;
-				$sum += $data['price'];
+				
 				}
 			echo '</table>';
 			echo '</div>';
