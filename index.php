0e9ef5574eec159d905c8d9e4fdb9cfc62ea8ffa
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-30 00:09:32 -0500

    Update index.php

--- index.php
+++ index.php
@@ -305,57 +305,57 @@
         //         }
         //     ];
 
-            function createCarouselItems($imageSets, $categoryName)
-            {
-                $carouselId = "carousel" . preg_replace('/\s+/', '', $categoryName);
-                echo '<h2>' . $categoryName . '</h2><hr>
-                      <div id="' . $carouselId . '" class="carousel slide" data-bs-ride="carousel">
-                      <div class="carousel-inner">';
-                $first = true;
-                 foreach ($images as $image) {
-		        $imageID = $image['ID'];
-		        $imageName = $image['name'];
-		        $imagePrice = $image['price'];
-		        $imagePath = $image['image'];
-		        $isInCart = $image['isInCart'];
+      //       function createCarouselItems($imageSets, $categoryName)
+      //       {
+      //           $carouselId = "carousel" . preg_replace('/\s+/', '', $categoryName);
+      //           echo '<h2>' . $categoryName . '</h2><hr>
+      //                 <div id="' . $carouselId . '" class="carousel slide" data-bs-ride="carousel">
+      //                 <div class="carousel-inner">';
+      //           $first = true;
+      //            foreach ($images as $image) {
+		    //     $imageID = $image['ID'];
+		    //     $imageName = $image['name'];
+		    //     $imagePrice = $image['price'];
+		    //     $imagePath = $image['image'];
+		    //     $isInCart = $image['isInCart'];
 		
-		        // Check if the item is already in the cart
-		        $buttonText = $isInCart ? 'In Cart' : 'Add to Cart';
-		        $buttonDisabled = $isInCart ? 'disabled' : '';
+		    //     // Check if the item is already in the cart
+		    //     $buttonText = $isInCart ? 'In Cart' : 'Add to Cart';
+		    //     $buttonDisabled = $isInCart ? 'disabled' : '';
 		
-		        echo '<div class="col-md-2">
-		                  <div class="card mb-3" style="cursor:pointer;" onclick="window.location.href=\'view-item.php?itemID=' . $imageID . '\'">
-		                      <img src="' . $imagePath . '" class="card-img-top" alt="Image of ' . $imageName . '" title="Click to view details">
-		                      <div class="card-body">
-		                          <h5 class="card-title">' . $imageName . '</h5>
-		                          <p class="card-text">$' . $imagePrice . '</p>
-		                          <form method="post" action="">
-		                              <input type="hidden" name="imageID" value="' . $imageID . '"> 
-		                              <input type="hidden" name="imageName" value="' . $imageName . '"> 
-		                              <input type="hidden" name="imagePrice" value="' . $imagePrice . '"> 
-		                              <button class="add-to-cart-btn" type="submit" name="submit" ' . $buttonDisabled . '>' . $buttonText . '</button>
-		                          </form>
-		                      </div>
-		                  </div>
-		              </div>';
-		    }
-                    echo '</div></div>';
-                    $first = false;
-                }
-                echo '</div>';
-                echo '
-		      <button class="carousel-control-prev" type="button" data-bs-target="#' . $carouselId . '" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                      </button>
+		    //     echo '<div class="col-md-2">
+		    //               <div class="card mb-3" style="cursor:pointer;" onclick="window.location.href=\'view-item.php?itemID=' . $imageID . '\'">
+		    //                   <img src="' . $imagePath . '" class="card-img-top" alt="Image of ' . $imageName . '" title="Click to view details">
+		    //                   <div class="card-body">
+		    //                       <h5 class="card-title">' . $imageName . '</h5>
+		    //                       <p class="card-text">$' . $imagePrice . '</p>
+		    //                       <form method="post" action="">
+		    //                           <input type="hidden" name="imageID" value="' . $imageID . '"> 
+		    //                           <input type="hidden" name="imageName" value="' . $imageName . '"> 
+		    //                           <input type="hidden" name="imagePrice" value="' . $imagePrice . '"> 
+		    //                           <button class="add-to-cart-btn" type="submit" name="submit" ' . $buttonDisabled . '>' . $buttonText . '</button>
+		    //                       </form>
+		    //                   </div>
+		    //               </div>
+		    //           </div>';
+		    // }
+      //               echo '</div></div>';
+      //               $first = false;
+      //           }
+      //           echo '</div>';
+      //           echo '
+		    //   <button class="carousel-control-prev" type="button" data-bs-target="#' . $carouselId . '" data-bs-slide="prev">
+      //                   <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      //                   <span class="visually-hidden">Previous</span>
+      //                 </button>
 		      
-                      <button class="carousel-control-next" type="button" data-bs-target="#' . $carouselId . '" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                      </button>
+      //                 <button class="carousel-control-next" type="button" data-bs-target="#' . $carouselId . '" data-bs-slide="next">
+      //                   <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      //                   <span class="visually-hidden">Next</span>
+      //                 </button>
 		      
-                    </div>';
-            }
+      //               </div>';
+      //       }
 
             foreach ($categories as $categoryName => $filter) {
                 $filteredImages = array_filter($images, $filter);
