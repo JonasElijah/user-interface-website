9bb1c43a2d69422d4d0811658bc7bba06703103d
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-30 00:34:49 -0500

    Update index.php

--- index.php
+++ index.php
@@ -310,59 +310,52 @@
                 }
             ];
 
-            function createCarouselItems($imageSets, $categoryName)
-            {
-                $carouselId = "carousel" . preg_replace('/\s+/', '', $categoryName);
-                echo '<h2>' . $categoryName . '</h2><hr>
-                      <div id="' . $carouselId . '" class="carousel slide" data-bs-ride="carousel">
-                      <div class="carousel-inner">';
-                $first = true;
-                 foreach ($images as $image) {
-		        $imageID = $image['ID'];
-		        $imageName = $image['name'];
-		        $imagePrice = $image['price'];
-		        $imagePath = $image['image'];
-		        $isInCart = $image['isInCart'];
+           function createCarouselItems($imageSets, $categoryName) {
+		    $carouselId = "carousel" . preg_replace('/\s+/', '', $categoryName);
+		    echo '<h2>' . htmlspecialchars($categoryName) . '</h2><hr>
+		          <div id="' . $carouselId . '" class="carousel slide" data-bs-ride="carousel">
+		          <div class="carousel-inner">';
 		
-		        $buttonText = $isInCart ? 'In Cart' : 'Add to Cart';
-		        $buttonDisabled = $isInCart ? 'disabled' : '';
-			  echo "Name: " . htmlspecialchars($imageName) . "<br/>";
-			    echo "Price: $" . htmlspecialchars($imagePrice) . "<br/>";
-			    echo "In Cart: " . ($isInCart ? 'Yes' : 'No') . "<br/><br/>";
+		    $first = true;
+		    foreach ($imageSets as $set) {
+		        $activeClass = $first ? 'active' : '';
+		        echo '<div class="carousel-item ' . $activeClass . '">
+		                <div class="row">';
+		        
+		        foreach ($set as $image) {
+		            $imageID = $image['ID'];
+		            $imageName = $image['name'];
+		            $imagePrice = $image['price'];
+		            $imagePath = $image['image'];
+		            $isInCart = $image['isInCart'];
+		            $buttonText = $isInCart ? 'In Cart' : 'Add to Cart';
+		            $buttonDisabled = $isInCart ? 'disabled' : '';
 		
-		        // echo '<div class="col-md-2">
-		        //           <div class="card mb-3" style="cursor:pointer;" onclick="window.location.href=\'view-item.php?itemID=' . $imageID . '\'">
-		        //               <img src="' . $imagePath . '" class="card-img-top" alt="Image of ' . $imageName . '" title="Click to view details">
-		        //               <div class="card-body">
-		        //                   <h5 class="card-title">' . $imageName . '</h5>
-		        //                   <p class="card-text">$' . $imagePrice . '</p>
-		        //                   <form method="post" action="">
-		        //                       <input type="hidden" name="imageID" value="' . $imageID . '"> 
-		        //                       <input type="hidden" name="imageName" value="' . $imageName . '"> 
-		        //                       <input type="hidden" name="imagePrice" value="' . $imagePrice . '"> 
-		        //                       <button class="add-to-cart-btn" type="submit" name="submit" ' . $buttonDisabled . '>' . $buttonText . '</button>
-		        //                   </form>
-		        //               </div>
-		        //           </div>
-		        //       </div>';
+		            echo '<div class="col-md-2">
+		                      <div class="card mb-3" onclick="window.location.href=\'view-item.php?itemID=' . $imageID . '\'">
+		                          <img src="' . htmlspecialchars($imagePath) . '" class="card-img-top" alt="Image of ' . htmlspecialchars($imageName) . '" title="' . htmlspecialchars($imageName) . '">
+		                          <div class="card-body">
+		                              <h5 class="card-title">' . htmlspecialchars($imageName) . '</h5>
+		                              <p class="card-text">$' . htmlspecialchars($imagePrice) . '</p>
+		                              <button class="add-to-cart-btn" ' . $buttonDisabled . '>' . $buttonText . '</button>
+		                          </div>
+		                      </div>
+		                  </div>';
+		        }
+		        echo '</div></div>';
+		        $first = false;
 		    }
-                    echo '</div></div>';
-                    $first = false;
-                
-                echo '</div>';
-                echo '
-		      <button class="carousel-control-prev" type="button" data-bs-target="#' . $carouselId . '" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                      </button>
-		      
-                      <button class="carousel-control-next" type="button" data-bs-target="#' . $carouselId . '" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                      </button>
-		      
-                    </div>';
-            }
+		    echo '</div>
+		          <button class="carousel-control-prev" type="button" data-bs-target="#' . $carouselId . '" data-bs-slide="prev">
+		              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		              <span class="visually-hidden">Previous</span>
+		          </button>
+		          <button class="carousel-control-next" type="button" data-bs-target="#' . $carouselId . '" data-bs-slide="next">
+		              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		              <span class="visually-hidden">Next</span>
+		          </button>
+		          </div>';
+		}
 
 
             foreach ($categories as $categoryName => $filter) {
