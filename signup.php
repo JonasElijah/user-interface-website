08a2cee2234aaf5cc10232b6f62e906a03cf3a08
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-04-29 22:41:37 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -86,79 +86,45 @@
 	?>
 	<div>
 	<header>
-      <nav
-        class="navbar navbar-expand-lg custom-navbar shadow rounded"
-      >
+    <nav class="navbar navbar-expand-lg custom-navbar shadow rounded">
         <div class="container-fluid">
-         <a href="index.php">
+	<a href="index.php">
 	    <img class="site-logo" src="assets/images/photography.png" alt="Photography Logo" style="max-width: 250px; max-height: 100px"/>
 	</a>
-
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-	<h1 class="text-center"> Photography Website </h1>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav ms-auto">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="gallery.php">Gallery</a>
-              </li>
-        
-		    <?php
-			    if(!isset($_SESSION['userID'])){
-			echo '<li class="nav-item"> ';
-               	 	echo ' <a class="nav-link" href="login.php">Login</a>';
-              		echo '</li>';
-              		echo '<li class="nav-item">';
-                	echo '<a class="nav-link" href="signup.php">Signup</a>';
-              		echo ' </li>';
-			       }
-			    else
-			    {
-			echo '<li class="nav-item">';
-                	echo '<a class="nav-link" href="account.php">Account</a>';
-             		 echo '</li>';
-			    }
-		?>
-		
-              
-              
-
-              <!--
-              <li class="nav-item dropdown" id="hover-dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  aria-haspopup="true"
-                  aria-expanded="false"
-                  >Profile</a
-                >
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li><a class="dropdown-item" href="#">Account</a></li>
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="#">Cart</a></li>
-                  <li><hr class="dropdown-divider" /></li>
-                  <li><a class="dropdown-item" href="#">Settings</a></li>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="gallery.php">Gallery</a>
+                    </li>
+                    <?php
+                    if(!isset($_SESSION['userID'])){
+                        echo '<li class="nav-item"> ';
+                        echo ' <a class="nav-link" href="login.php">Login</a>';
+                        echo '</li>';
+                        echo '<li class="nav-item">';
+                        echo '<a class="nav-link" href="signup.php">Signup</a>';
+                        echo ' </li>';
+                    }
+                    else{
+                        echo '<li class="nav-item">';
+                        echo '<a class="nav-link" href="cart.php">Cart</a>';
+                        echo '</li>';
+                        echo '<li class="nav-item">';
+                        echo '<a class="nav-link" href="account.php">Account</a>';
+                        echo '</li>';
+                    }
+                    ?>
                 </ul>
-              </li>
-              -->
-            </ul>
-          </div>
+            </div>
         </div>
-      </nav>
-    </header>
+    </nav>
+</header>
 </div>
 
 
