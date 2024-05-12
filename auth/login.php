9fefe21c8b2fe8d4190bbd0fa212ee0556af3a56
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-05-12 16:25:02 -0500

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -57,16 +57,21 @@ body::before {
 }
 
 .form-group button {
-    background-color: #28a745;
-    border-color: #28a745;
-    width: 30%;
-    color: #fff;
-    align: left;
-    padding: 10px 20px;
-    border-radius: 5px;
-    transition: background-color 0.3s, transform 0.3s;
+    background-color: #28a745; 
+    color: #fff; 
+    border: 1px solid #28a745;
+    border-radius: 5px; 
+    padding: 10px 20px; 
     font-size: 16px; 
+    font-weight: bold; 
+    text-transform: uppercase;
+    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); 
+    transition: background-color 0.3s, transform 0.3s, box-shadow 0.3s; 
+    cursor: pointer; 
+    display: inline-block;
+    margin-top: 10px; 
 }
+
 
 .btn:hover {
     background-color: #218838;
