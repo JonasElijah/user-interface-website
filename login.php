a87f8ae246f6c584803f8f55987a3393b3681dda
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-05-12 15:37:27 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,55 +1,77 @@
 <?php
 session_start();
+if(isset($_POST['submit'])){
+		echo '<div class ="col-md-6 offset-md-3 form">';
+		if($_POST['submit']=='submit'){
+			$errStatus=array();
+			
+			$email=$_POST['email'];
+			$pWord=$_POST['password'];
+			
+			
+			
+			
+			if ($email==NULL)
+			{
+				$errStatus[] .="email=emailNull";
+			}
+			elseif (!preg_match('/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/',$email))
+			{
+				$errStatus[] .="email=emailInvalid";
+			}
+			
+			$_SESSION['email']=$email;
 
-if (isset($_POST['submit'])) {
-    $errStatus = array();
+			if ($pWord==NULL)
+			{
+				$errStatus[] .="password=pWordNull";
+			}
+			
+			$_SESSION['password']=$pWord;
+			
 
-    $email = $_POST['email'];
-    $pWord = $_POST['password'];
+			
+			include("functions.php");
+			if (count($errStatus)>0)
+			{
+				$errString=implode("&",$errStatus);
+				redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/auth/login.php?$errString");
+			}
+			
+			$dblink = db_connect("UI-schema");
+			
+			$email = addslashes($email);
+			$pWord = addslashes($pWord);
 
-    if ($email == NULL) {
-        $errStatus[] = "email=emailNull";
-    } elseif (!preg_match('/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', $email)) {
-        $errStatus[] = "email=emailInvalid";
-    }
+			
+			
+			$sql = "SELECT `pWord`,`email`,`userID` FROM `user` where `email` LIKE '$email'";
+			$result = mysqli_query($dblink, $sql);
+			$row = $result->fetch_assoc();
+			if($row <0)
+			{
+			
+			redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/auth/login.php?email=emailNonexist");
+			}
+			else
+			{
+			
+			
+				if($row['pWord'] != $pWord)
+				{
+				redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/auth/login.php?password=pWordNonexist");
+				}
+				else
+				{
 
-    $_SESSION['email'] = $email;
-
-    if ($pWord == NULL) {
-        $errStatus[] = "password=pWordNull";
-    }
-
-    $_SESSION['password'] = $pWord;
-
-    include("functions.php");
-
-    if (count($errStatus) > 0) {
-        $errString = implode("&", $errStatus);
-        header("Location: login.php?$errString");
-        exit();
-    }
-
-    $dblink = db_connect("UI-schema");
-
-    $email = addslashes($email);
-    $pWord = addslashes($pWord);
-
-    $sql = "SELECT `pWord`, `email`, `userID` FROM `user` WHERE `email` LIKE '$email'";
-    $result = mysqli_query($dblink, $sql);
-    $row = $result->fetch_assoc();
-
-    if ($row < 0) {
-        header("Location: login.php?email=emailNonexist");
-        exit();
-    } else {
-        if ($row['pWord'] != $pWord) {
-            header("Location: login.php?password=pWordNonexist");
-            exit();
-        } else {
-            $_SESSION['userID'] = $row['userID'];
-            header("Location: index.php");
-            exit();
-        }
-    }
+			
+				$_SESSION['userID'] = $row['userID'];
+				//echo '$_SESSION[\'userID\'] = ' . $_SESSION['userID'] . '<br>';
+			
+				redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com");
+				}
+			}
+		}
+		
 }
 ?>
