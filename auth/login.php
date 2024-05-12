abbb31d29a7973738e276282497bcd3091b7dd11
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-05-12 16:12:16 -0500

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -8,7 +8,7 @@
 <link href="../node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
 
 <style>
- header {
+header {
     background-color: #fdf4eb;
 }
 
@@ -36,7 +36,7 @@ body::before {
 }
 
 .form {
-    background-color: rgba(253, 244, 235, 0.95);
+    background-color: rgba(253, 244, 235, 0.85); /* Semi-transparent background */
     padding: 50px;
     border-radius: 10px;
     box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
