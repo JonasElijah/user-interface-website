8a47aec46d80f66f35ef78d187570c74ef7d0b31
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-05-12 15:50:08 -0500

    Update index2.php

--- index2.php
+++ index2.php
@@ -2,6 +2,81 @@
     if (session_status() === PHP_SESSION_NONE) {
         session_start();
     }
+
+if(isset($_POST['submit'])){
+		echo '<div class ="col-md-6 offset-md-3 form">';
+		if($_POST['submit']=='submit'){
+			$errStatus=array();
+			
+			$email=$_POST['email'];
+			$pWord=$_POST['password'];
+			
+			
+			
+			
+			if ($email==NULL)
+			{
+				$errStatus[] .="email=emailNull";
+			}
+			elseif (!preg_match('/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/',$email))
+			{
+				$errStatus[] .="email=emailInvalid";
+			}
+			
+			$_SESSION['email']=$email;
+
+			if ($pWord==NULL)
+			{
+				$errStatus[] .="password=pWordNull";
+			}
+			
+			$_SESSION['password']=$pWord;
+			
+
+			
+			include("functions.php");
+			if (count($errStatus)>0)
+			{
+				$errString=implode("&",$errStatus);
+				redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/auth/login.php?$errString");
+			}
+			
+			$dblink = db_connect("UI-schema");
+			
+			$email = addslashes($email);
+			$pWord = addslashes($pWord);
+
+			
+			
+			$sql = "SELECT `pWord`,`email`,`userID` FROM `user` where `email` LIKE '$email'";
+			$result = mysqli_query($dblink, $sql);
+			$row = $result->fetch_assoc();
+			if($row <0)
+			{
+			
+			redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/auth/login.php?email=emailNonexist");
+			}
+			else
+			{
+			
+			
+				if($row['pWord'] != $pWord)
+				{
+				redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com/auth/login.php?password=pWordNonexist");
+				}
+				else
+				{
+
+			
+				$_SESSION['userID'] = $row['userID'];
+				//echo '$_SESSION[\'userID\'] = ' . $_SESSION['userID'] . '<br>';
+			
+				redirect("https://ec2-18-191-216-234.us-east-2.compute.amazonaws.com");
+				}
+			}
+		}
+		
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -413,7 +488,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <form id="loginForm" method="post" action="login.php">
+                <form id="loginForm" method="post" action="">
                     <div class="form-group">
                         <label for="email">Email address</label>
                         <input name="email" type="email" class="form-control" id="email" placeholder="Email">
