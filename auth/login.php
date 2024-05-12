1570a3ddd723b0065e28af6718c3bacdc9eaf784
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-05-12 17:03:53 -0500

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -66,8 +66,7 @@ body::before {
     border: 1px solid #28a745;
     border-radius: 5px; 
     padding: 10px 20px; 
-    font-size: 16px; 
-    font-weight: bold; 
+    font-size: 12px; 
     box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); 
     transition: background-color 0.3s, transform 0.3s, box-shadow 0.3s; 
     cursor: pointer; 
