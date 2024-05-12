97272bfa9948357bc8dc198207af59a496917ca8
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-05-12 17:34:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -379,11 +379,9 @@
 <br/>
 <br/>
 <br/>
-<footer class="footer mt-auto py-3 bg-light">
-    <div class="container text-center">
-        <span class="text-muted">Photography Website &copy; 2024</span>
-    </div>
-</footer>
+<div id="footer">
+        <?php include 'assets/html/footer.html'; ?>
+</div>
 <!-- Local Bootstrap JavaScript files -->
 <script src="node_modules/jquery/dist/jquery.slim.min.js"></script>
 <script src="node_modules/@popperjs/core/dist/umd/popper.min.js"></script>
