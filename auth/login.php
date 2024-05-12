dea01ccf69f07565fa2f897c9f90141ff2832878
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-05-12 16:45:04 -0500

    Update login.php

--- auth/login.php
+++ auth/login.php
@@ -245,7 +245,7 @@ if(!isset($_POST['submit'])){
 		
 			
 			echo '<br><button class="btn btn-success col-md-2 offset-md-5" name="submit" type="submit" value="submit">Log In</button>';
-			echo '<a href="/auth/signup.php" class="btn_signup btn-success col-md-2 offset-md-5">Sign Up</a></form>';
+			echo '<a href="/auth/signup.php" class="btn btn-success col-md-2 offset-md-5">Sign Up</a></form>';
 
 			
 }
