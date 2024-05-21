50ea7a6f155b02bc012f8271de9d43c05864d8b7
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-05-21 12:55:09 -0500

    update cart.php

--- cart/cart.php
+++ cart/cart.php
@@ -66,7 +66,6 @@
                     echo '<tr>';
                     echo '<td>'.$counter.'</td>';
                     echo '<td class="d-flex image-container"><img src="../'.$dataW['image'].'"><span>'.$data['name'].'</span></td>';
-                    echo '<td>'.$data['name'].'</td>';
 					echo '<td><form method="post" action=""><input type="hidden" name="remove_item_id" value="'.$data['imageID'].'"><button type="submit" class="btn btn-danger">Remove</button></form></td>';
                     echo '<td>Price: $'.$data['price'].'</td>';
                     echo '</tr>';
