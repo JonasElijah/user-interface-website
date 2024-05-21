27131e09fbc9231604c09ed42bdd45ffad74f27b
Jonas Elijah Icban <89656029+JonasElijah@users.noreply.github.com>
2024-05-21 12:26:02 -0500

    update cart.php

--- cart/cart.php
+++ cart/cart.php
@@ -41,7 +41,7 @@ if(!isset($_POST['submit']))
 			echo "<div class='row main-background'>";
 			echo '<div class = "col-md-9">';
 			echo '<br/><br/>';
-			echo '<h3 style = "color: dimgray; font-size: 45px; margin-left: 25px;"  align = "left">Shopping Cart </h3>';
+			echo '<h3 style = "color: rgb(86, 86, 86); font-size: 45px; margin-left: 25px;"  align = "left">Shopping Cart </h3>';
 			echo '<hr>';
 			echo '<table class = "table table-striped">';
 			echo '<tr>';
